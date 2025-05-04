b9e68f82d084ffb331c9bbff305a76254da72e95
Neil Jay Lacandazo <138986020+M-M-I-V@users.noreply.github.com>
2025-05-04 14:16:55 +0800

    Update users.sql

--- users.sql
+++ users.sql
@@ -1,13 +1,26 @@
+CREATE ROLE 'students';
+CREATE ROLE 'registrar_staffs';
+CREATE ROLE 'professors';
+CREATE ROLE 'admins';
+
+GRANT SELECT ON student_record_management_snippet.students TO 'students';
+GRANT SELECT, INSERT, UPDATE ON student_record_management_snippet.enrollments TO 'registrar_staffs';
+GRANT SELECT ON student_record_management_snippet.academic_records TO 'professors';
+GRANT ALL PRIVILEGES ON student_record_management_snippet.* TO 'admins' WITH GRANT OPTION;
+
 CREATE USER 'student'@'localhost' IDENTIFIED BY 'student';
-GRANT SELECT ON student_record_management_snippet.students TO 'student'@'localhost';
-
 CREATE USER 'registrar_staff'@'localhost' IDENTIFIED BY 'registrar';
-GRANT SELECT, INSERT, UPDATE ON student_record_management_snippet.enrollments TO 'registrar_staff'@'localhost';
-
 CREATE USER 'professor'@'localhost' IDENTIFIED BY 'professor';
-GRANT SELECT ON student_record_management_snippet.academic_records TO 'professor'@'localhost';
-
 CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin';
-GRANT ALL PRIVILEGES ON student_record_management_snippet.* TO 'admin'@'localhost' WITH GRANT OPTION;
+
+GRANT 'students' TO 'student'@'localhost';
+GRANT 'registrar_staffs' TO 'registrar_staff'@'localhost';
+GRANT 'professors' TO 'professor'@'localhost';
+GRANT 'admins' TO 'admin'@'localhost';
+
+SET DEFAULT ROLE 'students' TO 'student'@'localhost';
+SET DEFAULT ROLE 'registrar_staffs' TO 'registrar_staff'@'localhost';
+SET DEFAULT ROLE 'professors' TO 'professor'@'localhost';
+SET DEFAULT ROLE 'admins' TO 'admin'@'localhost';
 
 FLUSH PRIVILEGES;
