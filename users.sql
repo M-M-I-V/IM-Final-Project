591a54276a8475f0330116a12f4bcf5eb7909201
Neil Jay Lacandazo <138986020+M-M-I-V@users.noreply.github.com>
2025-05-04 13:30:01 +0800

    Update users.sql

--- users.sql
+++ users.sql
@@ -1,5 +1,13 @@
 CREATE USER 'student'@'localhost' IDENTIFIED BY 'student';
 GRANT SELECT ON student_record_management_snippet.students TO 'student'@'localhost';
 
+CREATE USER 'registrar_staff'@'localhost' IDENTIFIED BY 'registrar';
+GRANT SELECT, INSERT, UPDATE ON student_record_management_snippet.enrollments TO 'registrar_staff'@'localhost';
+
 CREATE USER 'professor'@'localhost' IDENTIFIED BY 'professor';
 GRANT SELECT ON student_record_management_snippet.academic_records TO 'professor'@'localhost';
+
+CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin';
+GRANT ALL PRIVILEGES ON student_record_management_snippet.* TO 'admin'@'localhost' WITH GRANT OPTION;
+
+FLUSH PRIVILEGES;
