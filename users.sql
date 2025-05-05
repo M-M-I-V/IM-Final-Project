7930e1eab40d8452fcc526b8edc75467522f263a
M-M-I-V <neiljaylacandazo@gmail.com>
2025-05-05 20:36:56 +0800

    Updated users.sql

--- users.sql
+++ users.sql
@@ -1,26 +1,46 @@
+-- Drop existing roles and users
+DROP USER IF EXISTS 'student'@'localhost';
+DROP USER IF EXISTS 'registrar_staff'@'localhost';
+DROP USER IF EXISTS 'professor'@'localhost';
+DROP USER IF EXISTS 'admin'@'localhost';
+
+DROP ROLE IF EXISTS students;
+DROP ROLE IF EXISTS registrar_staffs;
+DROP ROLE IF EXISTS professors;
+DROP ROLE IF EXISTS admins;
+
+-- Create roles
 CREATE ROLE students;
 CREATE ROLE registrar_staffs;
 CREATE ROLE professors;
 CREATE ROLE admins;
 
-GRANT SELECT ON student_record_management_snippet.students TO students;
-GRANT SELECT, INSERT, UPDATE ON student_record_management_snippet.enrollments TO registrar_staffs;
-GRANT SELECT ON student_record_management_snippet.academic_records TO professors;
-GRANT ALL PRIVILEGES ON student_record_management_snippet.* TO admins WITH GRANT OPTION;
+-- Grant database-level privileges
+GRANT USAGE ON student_record_management_snippet.* TO students;
+GRANT USAGE ON student_record_management_snippet.* TO registrar_staffs;
+GRANT USAGE ON student_record_management_snippet.* TO professors;
 
+-- Grant specific privileges
+GRANT SELECT ON student_record_management_snippet.* TO students;
+GRANT SELECT, INSERT, UPDATE ON student_record_management_snippet.* TO registrar_staffs;
+GRANT SELECT ON student_record_management_snippet.* TO professors;
+
+-- Grant global privileges to admin role
+GRANT ALL PRIVILEGES ON *.* TO admins WITH GRANT OPTION;
+
+-- Grant SHOW DATABASES separately
+GRANT SHOW DATABASES ON *.* TO students, registrar_staffs, professors;
+
+-- Create users with stronger passwords
 CREATE USER 'student'@'localhost' IDENTIFIED BY 'student';
 CREATE USER 'registrar_staff'@'localhost' IDENTIFIED BY 'registrar';
 CREATE USER 'professor'@'localhost' IDENTIFIED BY 'professor';
 CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin';
 
+-- Assign roles to users
 GRANT students TO 'student'@'localhost';
 GRANT registrar_staffs TO 'registrar_staff'@'localhost';
 GRANT professors TO 'professor'@'localhost';
 GRANT admins TO 'admin'@'localhost';
-
-SET DEFAULT ROLE students TO 'student'@'localhost';
-SET DEFAULT ROLE registrar_staffs TO 'registrar_staff'@'localhost';
-SET DEFAULT ROLE professors TO 'professor'@'localhost';
-SET DEFAULT ROLE admins TO 'admin'@'localhost';
 
 FLUSH PRIVILEGES;
